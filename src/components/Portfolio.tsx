242182268a8871a3229ae7b148138ae596b1f415
rak626 <rakeshacot@gmail.com>
2026-01-15 18:36:35 +0530

    changes for images

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,18 +1,9 @@
 import {JSX} from "react";
 import PortfolioCarousel from "@/components/PortfolioCarousel";
-import {getImages} from "@/lib/getImages";
-import {IMAGE_URL_PORTFOLIO_QUERY_1, IMAGE_URL_PORTFOLIO_QUERY_2} from "@/util/images";
+import {getImagesUrl} from "@/util/images";
 
 
 export default async function Portfolio(): Promise<JSX.Element> {
-    const imagesObjectArray = await getImages(IMAGE_URL_PORTFOLIO_QUERY_1);
-    const imagesObjectArray2 = await getImages(IMAGE_URL_PORTFOLIO_QUERY_2)
-    const images1 = imagesObjectArray
-        .map((img: { secure_url?: string }) => img.secure_url)
-        .filter(Boolean);
-    const images2 = imagesObjectArray2
-        .map((img: { secure_url?: string }) => img.secure_url)
-        .filter(Boolean);
-    const images = [...images1, ...images2];
+    const images = await getImagesUrl();
     return <PortfolioCarousel images={images}/>;
 }
--- src/util/images.ts
+++ src/util/images.ts
@@ -1,2 +1,16 @@
+import {getImages} from "@/lib/getImages";
+
 export const IMAGE_URL_PORTFOLIO_QUERY_1 = "folder:studio_kodak/portfolio1 AND resource_type:image AND type:upload";
 export const IMAGE_URL_PORTFOLIO_QUERY_2 = "folder:studio_kodak/portfolio2 AND resource_type:image AND type:upload";
+
+export const getImagesUrl = async () => {
+    const imagesObjectArray = await getImages(IMAGE_URL_PORTFOLIO_QUERY_1);
+    const imagesObjectArray2 = await getImages(IMAGE_URL_PORTFOLIO_QUERY_2)
+    const images1 = imagesObjectArray
+        .map((img: { secure_url?: string }) => img.secure_url)
+        .filter(Boolean);
+    const images2 = imagesObjectArray2
+        .map((img: { secure_url?: string }) => img.secure_url)
+        .filter(Boolean);
+    return [...images1, ...images2];
+}
